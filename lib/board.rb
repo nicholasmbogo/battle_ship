780f5d7a20056d909114b5f32630bf0773d584b6
NicholasMbogo <nicholasmbogo@yahoo.com>
2017-09-06 20:18:34 -0600

    finished validations on Board class

--- lib/board.rb
+++ lib/board.rb
@@ -26,7 +26,7 @@ class Board
     end_x_2_unit = start_x_2_unit+1
 
     # while @board[start_y_2_unit][start_x_2_unit] != "" || @board[end_y_2_unit][end_x_2_unit] != ""
-    until second_ship_valid?(start_y_2_unit, start_x_2_unit, end_y_2_unit, end_x_2_unit)
+    until ship_doesnot_overlap?(start_y_2_unit, start_x_2_unit, end_y_2_unit, end_x_2_unit)
       start_y_2_unit = rand(4)
       start_x_2_unit = rand(3)
       end_y_2_unit = start_y_2_unit
@@ -51,14 +51,25 @@ class Board
 
       end
     end
+  end
+
+  def determine_middle_value(start_y, start_x, ship_direction)
+    #binding.pry
+    if ship_direction == "horizontal"
+      @board[start_y][start_x+1]
+
+    elsif ship_direction == "vertical"
+      @board[start_y+1][start_x]
+    end
 
   end
 
-    def second_ship_valid?(start_y, start_x, end_y, end_x)
+  def ship_doesnot_overlap?(start_y, start_x, end_y, end_x)
       #binding.pry
-       @board[start_y][start_x] == "" && @board[end_y][end_x] == ""
-     rescue
-       return false
+      ship_direction = determine_ship_direction(start_y, start_x, end_y, end_x)
+       @board[start_y][start_x] == "" && @board[end_y][end_x] == "" && determine_middle_value(start_y, start_x, ship_direction) == ""
+     #rescue
+       #return false
     end
 
   def determine_ship_direction(start_y, start_x, end_y, end_x)
@@ -68,6 +79,59 @@ class Board
 
     elsif start_x == end_x
       return "vertical"
+
+    else
+      return false
     end
+  end
+
+  def ship_doesnot_wrap(start_y, start_x, end_y, end_x)
+    ship_direction = determine_ship_direction(start_y, start_x, end_y, end_x)
+
+    if ship_direction == "horizontal"
+      start_x < end_x
+
+    elsif ship_direction == "vertical"
+      start_y < end_y
+    end
+  end
+
+  def ship_direction_valid?(start_y, start_x, end_y, end_x)
+    ship_direction = determine_ship_direction(start_y, start_x, end_y, end_x)
+
+    if ship_direction
+      return true
+
+    else
+      return false
+    end
+  end
+
+  def ship_is_correct_length?(start_y, start_x, end_y, end_x, num_units)
+    ship_direction = determine_ship_direction(start_y, start_x, end_y, end_x)
+
+    if ship_direction == "horizontal"
+      end_x - start_x == num_units - 1
+
+    elsif ship_direction == "vertical"
+      end_y - start_y == num_units - 1
+    end
+  end
+
+  def translate_user_coordinates(user_coordinates)
+    split_coordinates = user_coordinates.split(" ")
+    y_map = {"A" => 0, "B" => 1, "C" => 2, "D" => 3}
+
+    translated = {start_y: nil, start_x: nil, end_y:nil, end_x: nil}
+
+    split_start = split_coordinates[0].chars
+    translated[:start_y] = y_map[split_start[0]]
+    translated[:start_x] = split_start[1].to_i - 1
+
+    split_end = split_coordinates[1].chars
+    translated[:end_y] = y_map[split_end[0]]
+    translated[:end_x] = split_end[1].to_i - 1
+    translated
+
   end
 end
--- test/board_test.rb
+++ test/board_test.rb
@@ -51,21 +51,91 @@ class BoardTest < MiniTest::Test
     assert_equal 2, board.board.flatten.delete_if{|value|value!=2}.length
   end
 
-  def test_can_validate_the_second_ship
+  def test_second_ship_doesnot_overlap
     board = Board.new
     board.place_ship(0,0, 0,2, 3)
 
-    assert_equal true, board.second_ship_valid?(3, 2, 2, 2)
+    assert_equal true, board.ship_doesnot_overlap?(2, 2, 3, 2)
   end
 
-  def test_a_ship_off_board_is_invalid
+  def test_second_3_unit_ship_doesnot_overlap
     board = Board.new
-    #board.place_ship(0,0, 0,2, 3)
+    board.place_ship(0,0, 0,2, 3)
 
-    assert_equal false, board.second_ship_valid?(3, 2, 4, 4)
+    assert_equal true, board.ship_doesnot_overlap?(1, 2, 3, 2)
   end
 
-  # def test_if_the_ship_next_to_each_other
-  #   board = Board.new
-  # end
+  def test_second_ship_does_overlap
+    board = Board.new
+    board.place_ship(0,0, 0,2, 3)
+
+    assert_equal false, board.ship_doesnot_overlap?(0, 0, 1, 0)
+  end
+
+  def test_second_3_unit_ship_does_overlap
+    board = Board.new
+    board.place_ship(0,0, 0,2, 3)
+
+    assert_equal false, board.ship_doesnot_overlap?(0, 1, 0, 3)
+  end
+
+  def test_ship_doesnot_wrap
+    board =Board.new
+
+    assert_equal true, board.ship_doesnot_wrap(0,0, 0,2)
+  end
+
+  def test_ship_does_wrap
+    board = Board.new
+
+    assert_equal false, board.ship_doesnot_wrap(0,2, 0,0)
+  end
+
+  def test_vertical_ship_doesnot_wrap
+    board =Board.new
+
+    assert_equal true, board.ship_doesnot_wrap(0,0, 2,0)
+  end
+
+  def test_vertical_ship_does_wrap
+    board = Board.new
+
+    assert_equal false, board.ship_doesnot_wrap(2,0, 0,0)
+  end
+
+  def test_ship_is_horizontal_or_vertical
+    board = Board.new
+
+    assert_equal true, board.ship_direction_valid?(0,0, 2,0)
+  end
+
+  def test_ship_is_not_horizontal_or_vertical
+    board = Board.new
+
+    assert_equal false, board.ship_direction_valid?(0,0, 2,2)
+  end
+
+  def test_ship_is_correct_length
+    board = Board.new
+
+    assert_equal true, board.ship_is_correct_length?(0,0, 0,1, 2)
+  end
+
+  def test_unit_3_ship_is_correct_length
+    board = Board.new
+
+    assert_equal true, board.ship_is_correct_length?(0,0, 2,0, 3)
+  end
+
+  def test_ship_is_incorrect_length
+    board = Board.new
+
+    assert_equal false, board.ship_is_correct_length?(1,0, 2,0, 3)
+  end
+
+  def test_can_translate_user_coordinates
+    board = Board.new
+
+    assert_equal ({start_y: 0, start_x: 0, end_y:0, end_x: 1}), board.translate_user_coordinates("A1 A2")
+  end
 end
